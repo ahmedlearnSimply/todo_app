65b92e3a343cd9ccdadfd349f6ca325786db7d78
mrrobot5-a <ahmedadel123422@gmail.com>
2025-05-25 18:37:11 +0300

    commit

---
+++ lib/core/util/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static final Color background = Color(0xff181818);
+  static final Color primaryText = Color(0xffFFFCFC);
+  static final Color secondaryText = Color(0xffC6C6C6);
+  static final Color green = Color(0xff15B86C);
+  static final Color surface = Color(0xff282828);
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:todo_app/core/util/color.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +8,52 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(backgroundColor: Colors.black),
+      child: Scaffold(
+        backgroundColor: AppColor.background,
+
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 21,
+                    backgroundImage: AssetImage(
+                      "assets/images/ahmed.png",
+                    ),
+                  ),
+                  Gap(8),
+                  Column(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Good Evening ,Usama ",
+                        style: TextStyle(
+                          color: AppColor.primaryText,
+                          fontFamily: 'poppins',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        "One task at a time.One step closer. ",
+                        style: TextStyle(
+                          color: AppColor.secondaryText,
+                          fontFamily: 'poppins',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
