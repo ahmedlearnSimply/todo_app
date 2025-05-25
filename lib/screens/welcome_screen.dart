bc71b74ddfc5f7aca88bf302e48dcdfb45a191d7
mrrobot5-a <ahmedadel123422@gmail.com>
2025-05-25 18:25:12 +0300

    commit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
+import 'package:todo_app/screens/home_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
-
+  WelcomeScreen({super.key});
+  final TextEditingController controller =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey =
+      GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -14,142 +18,160 @@ class WelcomeScreen extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: SizedBox(
-              child: Column(
-                crossAxisAlignment:
-                    CrossAxisAlignment.center,
-                children: [
-                  Gap(16),
-                  Row(
-                    mainAxisAlignment:
-                        MainAxisAlignment.center,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center,
+                  children: [
+                    Gap(16),
+                    Row(
+                      mainAxisAlignment:
+                          MainAxisAlignment.center,
 
-                    children: [
-                      SvgPicture.asset(
-                        "assets/images/logo.svg",
-                        height: 42,
-                        width: 42,
-                      ),
-                      Gap(16),
-                      Text(
-                        "Tasky",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: "plus",
-                          fontSize: 28,
-                          fontWeight: FontWeight.w400,
+                      children: [
+                        SvgPicture.asset(
+                          "assets/images/logo.svg",
+                          height: 42,
+                          width: 42,
                         ),
-                      ),
-                    ],
-                  ),
-                  Gap(100),
-                  Row(
-                    mainAxisAlignment:
-                        MainAxisAlignment.center,
-
-                    children: [
-                      Gap(16),
-                      Text(
-                        "Welcome To Tasky",
-                        style: TextStyle(
-                          color: Color(0xffFFFCFC),
-                          fontFamily: "plus",
-                          fontSize: 24,
-                          fontWeight: FontWeight.w400,
+                        Gap(16),
+                        Text(
+                          "Tasky",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "plus",
+                            fontSize: 28,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
-                      ),
-                      Gap(8),
-
-                      SvgPicture.asset(
-                        "assets/images/waving_hand.svg",
-                        height: 42,
-                        width: 42,
-                      ),
-                    ],
-                  ),
-                  Gap(8),
-                  Text(
-                    "Your productivity journey starts here.",
-                    style: TextStyle(
-                      color: Color(0xffFFFCFC),
-                      fontFamily: "plus",
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
+                      ],
                     ),
-                  ),
-                  Gap(24),
-                  SvgPicture.asset(
-                    "assets/images/person.svg",
-                    width: 216,
-                    height: 204,
-                  ),
-                  Gap(28),
-                  Row(
-                    children: [
-                      Text(
-                        "Full Name",
+                    Gap(100),
+                    Row(
+                      mainAxisAlignment:
+                          MainAxisAlignment.center,
+
+                      children: [
+                        Gap(16),
+                        Text(
+                          "Welcome To Tasky",
+                          style: TextStyle(
+                            color: Color(0xffFFFCFC),
+                            fontFamily: "plus",
+                            fontSize: 24,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Gap(8),
+
+                        SvgPicture.asset(
+                          "assets/images/waving_hand.svg",
+                          height: 42,
+                          width: 42,
+                        ),
+                      ],
+                    ),
+                    Gap(8),
+                    Text(
+                      "Your productivity journey starts here.",
+                      style: TextStyle(
+                        color: Color(0xffFFFCFC),
+                        fontFamily: "plus",
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Gap(24),
+                    SvgPicture.asset(
+                      "assets/images/person.svg",
+                      width: 216,
+                      height: 204,
+                    ),
+                    Gap(28),
+                    Row(
+                      children: [
+                        Text(
+                          "Full Name",
+                          style: TextStyle(
+                            color: Color(0xffFFFCFC),
+                            fontFamily: "poppins",
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Gap(8),
+                    TextFormField(
+                      controller: controller,
+                      validator: (String? value) {
+                        if (value == null ||
+                            value.trim().isEmpty) {
+                          return "Enter Your Full Name";
+                        }
+                        return null;
+                      },
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'poppins',
+                        fontSize: 18,
+                      ),
+                      decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 16,
+                            ),
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.circular(16),
+                          borderSide: BorderSide.none,
+                        ),
+                        hintText: "e.g. Sarah Khalid",
+                        hintStyle: TextStyle(
+                          fontFamily: 'poppins',
+                          color: Color(0xff6D6D6D),
+                        ),
+                        fillColor: Color(0xff282828),
+                        filled: true,
+                      ),
+                      cursorColor: Colors.white,
+                    ),
+                    Gap(24),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(
+                          MediaQuery.of(context).size.width,
+                          50,
+                        ),
+                        backgroundColor: Color(0xff15B86C),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!
+                            .validate()) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return HomeScreen();
+                              },
+                            ),
+                          );
+                        }
+                      },
+                      child: Text(
+                        "Let’s Get Started",
                         style: TextStyle(
                           color: Color(0xffFFFCFC),
                           fontFamily: "poppins",
                           fontSize: 16,
-                          fontWeight: FontWeight.w700,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                    ],
-                  ),
-                  Gap(8),
-                  TextField(
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'poppins',
-                      fontSize: 18,
                     ),
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 16,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                          16,
-                        ),
-                        borderSide: BorderSide.none,
-                      ),
-                      hintText: "e.g. Sarah Khalid",
-                      hintStyle: TextStyle(
-                        fontFamily: 'poppins',
-                        color: Color(0xff6D6D6D),
-                      ),
-                      fillColor: Color(0xff282828),
-                      filled: true,
-                    ),
-                    cursorColor: Colors.white,
-                  ),
-                  Gap(24),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(
-                        MediaQuery.of(context).size.width,
-                        50,
-                      ),
-                      backgroundColor: Color(0xff15B86C),
-                      // shape: RoundedRectangleBorder(
-                      //   borderRadius: BorderRadius.circular(
-                      //     22,
-                      //   ),
-                      // ),
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      "Let’s Get Started",
-                      style: TextStyle(
-                        color: Color(0xffFFFCFC),
-                        fontFamily: "poppins",
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
