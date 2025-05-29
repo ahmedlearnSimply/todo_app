c1f4130909a4ab624e50a5cbc8bc0c97cefa3f47
mrrobot5-a <ahmedadel123422@gmail.com>
2025-05-29 13:25:27 +0300

    commit

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -3,8 +3,16 @@ import 'package:gap/gap.dart';
 import 'package:todo_app/core/util/color.dart';
 
 class AddTask extends StatelessWidget {
-  const AddTask({super.key});
+  AddTask({super.key});
+  //Some Vars
+  final TextEditingController taskNameController =
+      TextEditingController();
 
+  final TextEditingController desController =
+      TextEditingController();
+
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  //TODO : Don't Forget to dispose the controllers
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -26,21 +34,140 @@ class AddTask extends StatelessWidget {
           backgroundColor: AppColor.background,
         ),
         body: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Task Name",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'poppins',
-                  fontWeight: FontWeight.w400,
-                  color: AppColor.primaryText,
+          padding: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+          child: Form(
+            key: _key,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Task Name",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'poppins',
+                    fontWeight: FontWeight.w400,
+                    color: AppColor.primaryText,
+                  ),
                 ),
-              ),
-              Gap(8),
-            ],
+                Gap(8),
+                TextFormField(
+                  validator: (String? value) {
+                    if (value == null ||
+                        value.trim().isEmpty) {
+                      return "Please Enter Name Of Task ";
+                    }
+                    return null;
+                  },
+                  controller: taskNameController,
+                  cursorColor: Colors.white,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'poppins',
+                    fontSize: 18,
+                  ),
+
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        16,
+                      ),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: AppColor.surface,
+                    filled: true,
+                    hintText:
+                        "Finish UI design for login screen",
+                    hintStyle: TextStyle(
+                      fontFamily: 'poppins',
+                      color: Color(0xff6D6D6D),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                Gap(20),
+                Text(
+                  "Task Description",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'poppins',
+                    fontWeight: FontWeight.w400,
+                    color: AppColor.primaryText,
+                  ),
+                ),
+                Gap(8),
+                TextFormField(
+                  validator: (String? value) {
+                    if (value == null ||
+                        value.trim().isEmpty) {
+                      return "Please Enter Description Of Task ";
+                    }
+                    return null;
+                  },
+                  controller: desController,
+                  cursorColor: Colors.white,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'poppins',
+                    fontSize: 18,
+                  ),
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        16,
+                      ),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: AppColor.surface,
+                    filled: true,
+                    hintText:
+                        "Finish onboarding UI and hand off to devs by Thursday.",
+                    hintStyle: TextStyle(
+                      fontFamily: 'poppins',
+                      color: Color(0xff6D6D6D),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                Spacer(),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: AppColor.primaryText,
+                    backgroundColor: AppColor.green,
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      40,
+                    ),
+                  ),
+                  icon: Icon(Icons.add),
+                  onPressed: () {},
+
+                  label: Text(
+                    "Add Task",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: 'poppins',
+                      fontWeight: FontWeight.w500,
+                      color: AppColor.primaryText,
+                    ),
+                  ),
+                ),
+                Gap(8),
+              ],
+            ),
           ),
         ),
       ),
