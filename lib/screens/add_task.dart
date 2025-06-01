94df69eef6f2040b5181cfe3bf520cae3cc666f3
mrrobot5-a <ahmedadel123422@gmail.com>
2025-06-01 18:06:08 +0300

    commit

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -24,154 +24,49 @@ class _AddTaskState extends State<AddTask> {
   //TODO : Don't Forget to dispose the controllers
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: AppColor.background,
-        appBar: AppBar(
-          surfaceTintColor: AppColor.background,
-          title: Text(
-            "New Task",
-            style: TextStyle(
-              fontSize: 20,
-              fontFamily: 'poppins',
-              fontWeight: FontWeight.w400,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: AppColor.background,
+          appBar: AppBar(
+            surfaceTintColor: AppColor.background,
+            title: Text(
+              "New Task",
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'poppins',
+                fontWeight: FontWeight.w400,
+                color: AppColor.primaryText,
+              ),
+            ),
+            iconTheme: IconThemeData(
               color: AppColor.primaryText,
             ),
+            backgroundColor: AppColor.background,
           ),
-          iconTheme: IconThemeData(
-            color: AppColor.primaryText,
-          ),
-          backgroundColor: AppColor.background,
-        ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-          child: Form(
-            key: _key,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Gap(8),
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            "Task Name",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontFamily: 'poppins',
-                              fontWeight: FontWeight.w400,
-                              color: AppColor.primaryText,
-                            ),
-                          ),
-                        ),
-                        Gap(8),
-
-                        TextFormField(
-                          validator: (String? value) {
-                            if (value == null ||
-                                value.trim().isEmpty) {
-                              return "Please Enter Name Of Task ";
-                            }
-                            return null;
-                          },
-                          controller: taskNameController,
-                          cursorColor: Colors.white,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'poppins',
-                            fontSize: 18,
-                          ),
-
-                          decoration: InputDecoration(
-                            contentPadding:
-                                EdgeInsets.symmetric(
-                                  horizontal: 16,
-                                  vertical: 16,
-                                ),
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.circular(16),
-                              borderSide: BorderSide.none,
-                            ),
-                            fillColor: AppColor.surface,
-                            filled: true,
-                            hintText:
-                                "Finish UI design for login screen",
-                            hintStyle: TextStyle(
-                              fontFamily: 'poppins',
-                              color: Color(0xff6D6D6D),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                        Gap(20),
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            "Task Description",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontFamily: 'poppins',
-                              fontWeight: FontWeight.w400,
-                              color: AppColor.primaryText,
-                            ),
-                          ),
-                        ),
-                        Gap(8),
-                        TextFormField(
-                          validator: (String? value) {
-                            if (value == null ||
-                                value.trim().isEmpty) {
-                              return "Please Enter Description Of Task ";
-                            }
-                            return null;
-                          },
-                          controller: desController,
-                          cursorColor: Colors.white,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'poppins',
-                            fontSize: 18,
-                          ),
-                          maxLines: 5,
-                          decoration: InputDecoration(
-                            contentPadding:
-                                EdgeInsets.symmetric(
-                                  horizontal: 16,
-                                  vertical: 16,
-                                ),
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.circular(16),
-                              borderSide: BorderSide.none,
-                            ),
-                            fillColor: AppColor.surface,
-                            filled: true,
-                            hintText:
-                                "Finish onboarding UI and hand off to devs by Thursday.",
-                            hintStyle: TextStyle(
-                              fontFamily: 'poppins',
-                              color: Color(0xff6D6D6D),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                        Gap(20),
-                        Row(
-                          mainAxisAlignment:
-                              MainAxisAlignment
-                                  .spaceBetween,
-                          children: [
-                            Text(
-                              "High Priority",
+          body: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+            child: Form(
+              key: _key,
+              child: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Gap(8),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "Task Name",
                               style: TextStyle(
                                 fontSize: 16,
                                 fontFamily: 'poppins',
@@ -179,47 +74,167 @@ class _AddTaskState extends State<AddTask> {
                                 color: AppColor.primaryText,
                               ),
                             ),
-                            Switch(
-                              value: isHighPriority,
-                              activeTrackColor:
-                                  AppColor.green,
+                          ),
+                          Gap(8),
 
-                              onChanged: (value) {
-                                setState(() {
-                                  isHighPriority = value;
-                                });
-                              },
+                          TextFormField(
+                            validator: (String? value) {
+                              if (value == null ||
+                                  value.trim().isEmpty) {
+                                return "Please Enter Name Of Task ";
+                              }
+                              return null;
+                            },
+                            controller: taskNameController,
+                            cursorColor: Colors.white,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'poppins',
+                              fontSize: 18,
                             ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: AppColor.primaryText,
-                    backgroundColor: AppColor.green,
-                    fixedSize: Size(
-                      MediaQuery.of(context).size.width,
-                      40,
-                    ),
-                  ),
-                  icon: Icon(Icons.add),
-                  onPressed: () {},
 
-                  label: Text(
-                    "Add Task",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontFamily: 'poppins',
-                      fontWeight: FontWeight.w500,
-                      color: AppColor.primaryText,
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.symmetric(
+                                    horizontal: 16,
+                                    vertical: 16,
+                                  ),
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.circular(
+                                      16,
+                                    ),
+                                borderSide: BorderSide.none,
+                              ),
+                              fillColor: AppColor.surface,
+                              filled: true,
+                              hintText:
+                                  "Finish UI design for login screen",
+                              hintStyle: TextStyle(
+                                fontFamily: 'poppins',
+                                color: Color(0xff6D6D6D),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          Gap(20),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "Task Description",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'poppins',
+                                fontWeight: FontWeight.w400,
+                                color: AppColor.primaryText,
+                              ),
+                            ),
+                          ),
+                          Gap(8),
+                          TextFormField(
+                            validator: (String? value) {
+                              if (value == null ||
+                                  value.trim().isEmpty) {
+                                return "Please Enter Description Of Task ";
+                              }
+                              return null;
+                            },
+                            controller: desController,
+                            cursorColor: Colors.white,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'poppins',
+                              fontSize: 18,
+                            ),
+                            maxLines: 5,
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.symmetric(
+                                    horizontal: 16,
+                                    vertical: 16,
+                                  ),
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.circular(
+                                      16,
+                                    ),
+                                borderSide: BorderSide.none,
+                              ),
+                              fillColor: AppColor.surface,
+                              filled: true,
+                              hintText:
+                                  "Finish onboarding UI and hand off to devs by Thursday.",
+                              hintStyle: TextStyle(
+                                fontFamily: 'poppins',
+                                color: Color(0xff6D6D6D),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          Gap(20),
+                          Row(
+                            mainAxisAlignment:
+                                MainAxisAlignment
+                                    .spaceBetween,
+                            children: [
+                              Text(
+                                "High Priority",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'poppins',
+                                  fontWeight:
+                                      FontWeight.w400,
+                                  color:
+                                      AppColor.primaryText,
+                                ),
+                              ),
+                              Switch(
+                                value: isHighPriority,
+                                activeTrackColor:
+                                    AppColor.green,
+
+                                onChanged: (value) {
+                                  setState(() {
+                                    isHighPriority = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Gap(8),
-              ],
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: AppColor.primaryText,
+                      backgroundColor: AppColor.green,
+                      fixedSize: Size(
+                        MediaQuery.of(context).size.width,
+                        40,
+                      ),
+                    ),
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      if (_key.currentState?.validate() ??
+                          false) {}
+                    },
+
+                    label: Text(
+                      "Add Task",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontFamily: 'poppins',
+                        fontWeight: FontWeight.w500,
+                        color: AppColor.primaryText,
+                      ),
+                    ),
+                  ),
+                  Gap(8),
+                ],
+              ),
             ),
           ),
         ),
