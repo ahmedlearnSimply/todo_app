464998ec666171e00b2f8791ad9ed737a5a13969
mrrobot5-a <ahmedadel123422@gmail.com>
2025-06-12 00:34:00 +0300

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -161,9 +161,23 @@ class _HomeScreenState extends State<HomeScreen> {
                   SvgPicture.asset("assets/images/waving_hand.svg", width: 32, height: 32),
                 ],
               ),
+              Gap(24),
+              Text(
+                "My Tasks",
+                style: TextStyle(
+                  color: AppColor.primaryText,
+                  fontFamily: 'poppins',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              Gap(16),
+
               if (allTasks.isNotEmpty)
                 Expanded(
-                  child: ListView.builder(
+                  child: ListView.separated(
+                    padding: EdgeInsets.only(bottom: 50),
+                    separatorBuilder: (BuildContext context, int index) => Gap(8),
                     itemCount: allTasks.length,
                     itemBuilder: (BuildContext context, int index) {
                       return Padding(
@@ -196,49 +210,79 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                               ),
                               Gap(8),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      allTasks[index].taskName,
+                              (allTasks[index].taskDescription != "")
+                                  ? Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          allTasks[index].taskName,
 
-                                      style: TextStyle(
-                                        color:
-                                            allTasks[index].isDone
-                                                ? Color(0xffA0A0A0)
-                                                : AppColor.primaryText,
-                                        fontFamily: 'poppins',
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 16,
+                                          style: TextStyle(
+                                            color:
+                                                allTasks[index].isDone
+                                                    ? Color(0xffA0A0A0)
+                                                    : AppColor.primaryText,
+                                            fontFamily: 'poppins',
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 16,
 
-                                        overflow: TextOverflow.ellipsis,
-                                        decoration:
-                                            allTasks[index].isDone
-                                                ? TextDecoration.lineThrough
-                                                : TextDecoration.none,
-                                        decorationColor: Color(0xffA0A0A0),
-                                      ),
-                                      maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                            decoration:
+                                                allTasks[index].isDone
+                                                    ? TextDecoration.lineThrough
+                                                    : TextDecoration.none,
+                                            decorationColor: Color(0xffA0A0A0),
+                                          ),
+                                          maxLines: 1,
+                                        ),
+                                        Text(
+                                          allTasks[index].taskDescription,
+                                          style: TextStyle(
+                                            color:
+                                                allTasks[index].isDone
+                                                    ? Color(0xffA0A0A0)
+                                                    : AppColor.secondaryText,
+                                            fontFamily: 'poppins',
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 14,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          maxLines: 1,
+                                        ),
+                                      ],
                                     ),
-                                    Text(
-                                      allTasks[index].taskDescription,
-                                      style: TextStyle(
-                                        color:
-                                            allTasks[index].isDone
-                                                ? Color(0xffA0A0A0)
-                                                : AppColor.secondaryText,
-                                        fontFamily: 'poppins',
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 14,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                      maxLines: 1,
+                                  )
+                                  : Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          allTasks[index].taskName,
+
+                                          style: TextStyle(
+                                            color:
+                                                allTasks[index].isDone
+                                                    ? Color(0xffA0A0A0)
+                                                    : AppColor.primaryText,
+                                            fontFamily: 'poppins',
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 16,
+
+                                            overflow: TextOverflow.ellipsis,
+                                            decoration:
+                                                allTasks[index].isDone
+                                                    ? TextDecoration.lineThrough
+                                                    : TextDecoration.none,
+                                            decorationColor: Color(0xffA0A0A0),
+                                          ),
+                                          maxLines: 1,
+                                        ),
+                                      ],
                                     ),
-                                  ],
-                                ),
-                              ),
+                                  ),
                               Gap(12),
                               IconButton(
                                 onPressed: () {},
