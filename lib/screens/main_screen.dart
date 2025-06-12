f8111efd5f257079fce26174251e0786d53e4ecb
mrrobot5-a <ahmedadel123422@gmail.com>
2025-06-13 01:07:36 +0300

    commitolorFilter.mode(AppColor.green, BlendMode.srcIn)

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:todo_app/core/util/color.dart';
 import 'package:todo_app/screens/completed_tasks.dart';
 import 'package:todo_app/screens/home_screen.dart';
+import 'package:todo_app/screens/profile_screen.dart';
 import 'package:todo_app/screens/todo_screen.dart';
 
 class MainScreen extends StatefulWidget {
@@ -12,38 +13,71 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-int _currentIndex = 0;
-List<Widget> screens = [HomeScreen(), TodoScreen(), CompletedTasks()];
-
 class _MainScreenState extends State<MainScreen> {
+  List<Widget> screens = [HomeScreen(), TodoScreen(), CompletedTasks(), ProfileScreen()];
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         onTap: (int index) {
-          _currentIndex = index;
+          setState(() {
+            _currentIndex = index;
+          });
         },
         type: BottomNavigationBarType.fixed,
         elevation: 0,
         backgroundColor: AppColor.background,
-        unselectedItemColor: Colors.white,
+        unselectedItemColor: AppColor.primaryText,
+        selectedItemColor: AppColor.green,
         selectedLabelStyle: TextStyle(fontFamily: 'poppins', fontSize: 12),
-        fixedColor: AppColor.background,
         items: [
-          BottomNavigationBarItem(icon: SvgPicture.asset("assets/images/home.svg"), label: "Home"),
-          BottomNavigationBarItem(icon: SvgPicture.asset("assets/images/todo.svg"), label: "To Do"),
           BottomNavigationBarItem(
-            icon: SvgPicture.asset("assets/images/completed.svg"),
+            icon: SvgPicture.asset(
+              "assets/images/home.svg",
+              colorFilter:
+                  _currentIndex == 0
+                      ? ColorFilter.mode(AppColor.green, BlendMode.srcIn)
+                      : ColorFilter.mode(AppColor.primaryText, BlendMode.srcIn),
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/images/todo.svg",
+              colorFilter:
+                  _currentIndex == 1
+                      ? ColorFilter.mode(AppColor.green, BlendMode.srcIn)
+                      : ColorFilter.mode(AppColor.primaryText, BlendMode.srcIn),
+            ),
+            label: "To Do",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/images/completed.svg",
+              colorFilter:
+                  _currentIndex == 2
+                      ? ColorFilter.mode(AppColor.green, BlendMode.srcIn)
+                      : ColorFilter.mode(AppColor.primaryText, BlendMode.srcIn),
+            ),
             label: "Completed",
           ),
           BottomNavigationBarItem(
-            icon: SvgPicture.asset("assets/images/profile.svg"),
+            icon: SvgPicture.asset(
+              "assets/images/profile.svg",
+              colorFilter:
+                  _currentIndex == 3
+                      ? ColorFilter.mode(AppColor.green, BlendMode.srcIn)
+                      : ColorFilter.mode(AppColor.primaryText, BlendMode.srcIn),
+            ),
             label: "Profile",
           ),
         ],
       ),
 
-      body: Center(child: Text("Main Screen")),
+      body: screens[_currentIndex],
     );
   }
 }
