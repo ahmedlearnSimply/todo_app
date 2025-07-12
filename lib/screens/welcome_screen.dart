1e5cbe7953e6be2f78abb882b8b4bc020050e5e5
mrrobot5-a <ahmedadel123422@gmail.com>
2025-07-12 13:08:01 +0300

    commit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:todo_app/core/services/app_local_storage.dart';
 import 'package:todo_app/core/util/color.dart';
+import 'package:todo_app/core/widgets/custom_text_form.dart';
 import 'package:todo_app/screens/home_screen.dart';
 import 'package:todo_app/screens/main_screen.dart';
 
@@ -18,7 +19,9 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        FocusScope.of(context).unfocus(); // to remove keyboard when you press on pixel in screen
+        FocusScope.of(
+          context,
+        ).unfocus(); // to remove keyboard when you press on pixel in screen
       },
       child: SafeArea(
         child: Scaffold(
@@ -37,7 +40,11 @@ class WelcomeScreen extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
 
                         children: [
-                          SvgPicture.asset("assets/images/logo.svg", height: 42, width: 42),
+                          SvgPicture.asset(
+                            "assets/images/logo.svg",
+                            height: 42,
+                            width: 42,
+                          ),
                           Gap(16),
                           Text(
                             "Tasky",
@@ -67,7 +74,11 @@ class WelcomeScreen extends StatelessWidget {
                           ),
                           Gap(8),
 
-                          SvgPicture.asset("assets/images/waving_hand.svg", height: 42, width: 42),
+                          SvgPicture.asset(
+                            "assets/images/waving_hand.svg",
+                            height: 42,
+                            width: 42,
+                          ),
                         ],
                       ),
                       Gap(8),
@@ -81,7 +92,11 @@ class WelcomeScreen extends StatelessWidget {
                         ),
                       ),
                       Gap(24),
-                      SvgPicture.asset("assets/images/person.svg", width: 216, height: 204),
+                      SvgPicture.asset(
+                        "assets/images/person.svg",
+                        width: 216,
+                        height: 204,
+                      ),
                       Gap(28),
                       Row(
                         children: [
@@ -97,37 +112,31 @@ class WelcomeScreen extends StatelessWidget {
                         ],
                       ),
                       Gap(8),
-                      TextFormField(
+                      CustomTextForm(
                         controller: textController,
-                        validator: (String? value) {
+                        hintText: "e.g. Ahmed Adel",
+                        validator: (value) {
                           if (value == null || value.trim().isEmpty) {
-                            return "Enter Your Full Name";
+                            return "Enter Your Name";
                           }
                           return null;
                         },
-                        style: TextStyle(color: Colors.white, fontFamily: 'poppins', fontSize: 18),
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: BorderSide.none,
-                          ),
-                          hintText: "e.g. Sarah Khalid",
-                          hintStyle: TextStyle(fontFamily: 'poppins', color: Color(0xff6D6D6D)),
-                          fillColor: Color(0xff282828),
-                          filled: true,
-                        ),
-                        cursorColor: Colors.white,
                       ),
+
                       Gap(24),
                       ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          minimumSize: Size(MediaQuery.of(context).size.width, 50),
+                          minimumSize: Size(
+                            MediaQuery.of(context).size.width,
+                            50,
+                          ),
                           backgroundColor: AppColor.green,
                         ),
                         onPressed: () async {
                           if (_formKey.currentState!.validate()) {
-                            await AppLocalStorage.saveName(textController.value.text);
+                            await AppLocalStorage.saveName(
+                              textController.value.text,
+                            );
                             log(textController.value.text);
                             await AppLocalStorage.saveOnboarding(true);
 
