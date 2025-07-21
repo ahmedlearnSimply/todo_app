9160d5c3845542f025e19fbaf15eef6edf4f3c9f
mrrobot5-a <ahmedadel123422@gmail.com>
2025-07-21 05:16:40 +0300

    commit

---
+++ lib/core/theme/dark_theme.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/core/util/color.dart';
+
+ThemeData darkTheme = ThemeData(
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: WidgetStateProperty.all(AppColor.green),
+      foregroundColor: WidgetStateProperty.all(AppColor.primaryText),
+    ),
+  ),
+  switchTheme: SwitchThemeData(
+    thumbColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return AppColor.primaryText;
+      }
+      return Color(0xff9E9E9E);
+    }),
+    trackColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return AppColor.green;
+      }
+      return Colors.transparent;
+    }),
+    trackOutlineColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.transparent;
+      }
+      return Color(0xff9E9E9E);
+    }),
+    trackOutlineWidth: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return 0;
+      }
+      return 2;
+    }),
+  ),
+  scaffoldBackgroundColor: AppColor.background,
+  appBarTheme: AppBarTheme(
+    centerTitle: false,
+    elevation: 0,
+    iconTheme: IconThemeData(color: AppColor.primaryText),
+    titleTextStyle: TextStyle(
+      fontSize: 20,
+      fontFamily: 'poppins',
+      fontWeight: FontWeight.w400,
+      color: AppColor.primaryText,
+    ),
+    surfaceTintColor: AppColor.background,
+    backgroundColor: AppColor.background,
+  ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:todo_app/core/services/app_local_storage.dart';
+import 'package:todo_app/core/theme/dark_theme.dart';
 import 'package:todo_app/core/util/color.dart';
 import 'package:todo_app/screens/main_screen.dart';
 import 'package:todo_app/screens/welcome_screen.dart';
@@ -21,54 +22,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(AppColor.green),
-            foregroundColor: WidgetStateProperty.all(AppColor.primaryText),
-          ),
-        ),
-        switchTheme: SwitchThemeData(
-          thumbColor: WidgetStateProperty.resolveWith((states) {
-            if (states.contains(WidgetState.selected)) {
-              return AppColor.primaryText;
-            }
-            return Color(0xff9E9E9E);
-          }),
-          trackColor: WidgetStateProperty.resolveWith((states) {
-            if (states.contains(WidgetState.selected)) {
-              return AppColor.green;
-            }
-            return Colors.transparent;
-          }),
-          trackOutlineColor: WidgetStateProperty.resolveWith((states) {
-            if (states.contains(WidgetState.selected)) {
-              return Colors.transparent;
-            }
-            return Color(0xff9E9E9E);
-          }),
-          trackOutlineWidth: WidgetStateProperty.resolveWith((states) {
-            if (states.contains(WidgetState.selected)) {
-              return 0;
-            }
-            return 2;
-          }),
-        ),
-        scaffoldBackgroundColor: AppColor.background,
-        appBarTheme: AppBarTheme(
-          centerTitle: false,
-          elevation: 0,
-          iconTheme: IconThemeData(color: AppColor.primaryText),
-          titleTextStyle: TextStyle(
-            fontSize: 20,
-            fontFamily: 'poppins',
-            fontWeight: FontWeight.w400,
-            color: AppColor.primaryText,
-          ),
-          surfaceTintColor: AppColor.background,
-          backgroundColor: AppColor.background,
-        ),
-      ),
+      theme: darkTheme,
       debugShowCheckedModeBanner: false,
       home: isFirstTime ? MainScreen() : WelcomeScreen(),
     );
