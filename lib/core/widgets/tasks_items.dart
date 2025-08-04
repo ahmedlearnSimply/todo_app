90930ec7948c0ea600938eb1ec9d5eb457caab7e
mrrobot5-a <ahmedadel123422@gmail.com>
2025-08-04 23:55:53 +0300

    commit

--- lib/core/widgets/tasks_items.dart
+++ lib/core/widgets/tasks_items.dart
@@ -1,28 +1,83 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:todo_app/core/enum/task_item_enum.dart';
 import 'package:todo_app/core/models/task_model.dart';
+import 'package:todo_app/core/services/app_local_storage.dart';
 import 'package:todo_app/core/theme/theme_controller.dart';
 import 'package:todo_app/core/util/color.dart';
 
-class TasksItems extends StatelessWidget {
+class TasksItems extends StatefulWidget {
   TasksItems({
     super.key,
     required this.tasks,
     required this.isLoading,
     required this.onTap,
     this.emptyMessage,
+    this.onDelete,
   });
 
   List<TaskModel> tasks = [];
   bool isLoading = false;
   final Function(bool? value, int index) onTap;
   String? emptyMessage;
+  final void Function(int id)? onDelete;
+
+  @override
+  State<TasksItems> createState() => _TasksItemsState();
+}
+
+class _TasksItemsState extends State<TasksItems> {
+  void doneTasks(bool? value, int index) async {
+    setState(() {
+      widget.tasks[index].isDone = value ?? false;
+    });
+    await saveUpdatedTasks();
+  }
+
+  Future<void> saveUpdatedTasks() async {
+    final updatedTasks =
+        widget.tasks.map((element) => element.toJson()).toList();
+    AppLocalStorage.setString('tasks', jsonEncode(updatedTasks));
+    // calculateIndicator();
+  }
+
+  void _loadTask() async {
+    setState(() {
+      widget.isLoading = true;
+    });
+
+    final finalTask = AppLocalStorage.getString('tasks');
+    if (finalTask != null) {
+      final taskAfterDecode = jsonDecode(finalTask) as List<dynamic>;
+
+      setState(() {
+        widget.tasks =
+            taskAfterDecode.map((e) => TaskModel.fromJson(e)).toList();
+        final completedTasks =
+            widget.tasks.where((element) => element.isDone).toList();
+      });
+      setState(() {
+        widget.isLoading = false;
+      });
+    }
+  }
+
+  void deleteTask(int id) async {
+    setState(() {
+      widget.tasks.removeWhere((task) => task.id == id);
+    });
+
+    await saveUpdatedTasks();
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (tasks.isEmpty && !isLoading) {
+    if (widget.tasks.isEmpty && !widget.isLoading) {
       return Center(
         child: Text(
-          emptyMessage ?? "No Task Found",
+          widget.emptyMessage ?? "No Task Found",
           style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 26),
         ),
       );
@@ -30,7 +85,7 @@ class TasksItems extends StatelessWidget {
     return ListView.separated(
       padding: EdgeInsets.only(bottom: 50),
       separatorBuilder: (BuildContext context, int index) => Gap(8),
-      itemCount: tasks.length,
+      itemCount: widget.tasks.length,
       itemBuilder: (BuildContext context, int index) {
         return Padding(
           padding: const EdgeInsets.only(top: 8.0),
@@ -56,10 +111,10 @@ class TasksItems extends StatelessWidget {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(4),
                     ),
-                    value: tasks[index].isDone,
+                    value: widget.tasks[index].isDone,
 
                     onChanged: (bool? value) {
-                      onTap(value, index);
+                      widget.onTap(value, index);
                     },
                   ),
                 ),
@@ -70,27 +125,27 @@ class TasksItems extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        tasks[index].taskName,
+                        widget.tasks[index].taskName,
                         //!-----//
                         style: Theme.of(
                           context,
                         ).textTheme.titleMedium!.copyWith(
                           color:
-                              tasks[index].isDone
+                              widget.tasks[index].isDone
                                   ? Theme.of(context).disabledColor
                                   : Theme.of(context).hintColor,
                           overflow: TextOverflow.ellipsis,
                           decoration:
-                              tasks[index].isDone
+                              widget.tasks[index].isDone
                                   ? TextDecoration.lineThrough
                                   : TextDecoration.none,
                           decorationColor: Theme.of(context).disabledColor,
                         ),
                         maxLines: 1,
                       ),
-                      if (tasks[index].taskDescription != "")
+                      if (widget.tasks[index].taskDescription != "")
                         Text(
-                          tasks[index].taskDescription,
+                          widget.tasks[index].taskDescription,
                           style:
                               Theme.of(
                                 context,
@@ -103,16 +158,43 @@ class TasksItems extends StatelessWidget {
                 ),
 
                 Gap(12),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
+                PopupMenuButton<TaskItemEnum>(
+                  itemBuilder:
+                      (context) =>
+                          TaskItemEnum.values.map((e) {
+                            return PopupMenuItem(
+                              value: e,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [Text(e.name), Divider()],
+                              ),
+                            );
+                          }).toList(),
+                  //!---------------4-8-2025-----------------//
+                  onSelected: (value) {
+                    switch (value) {
+                      case TaskItemEnum.markAsDone:
+                        bool newValue = !widget.tasks[index].isDone;
+                        doneTasks(newValue, index);
+                        break;
+                      case TaskItemEnum.edit:
+                        print("Edit");
+                        break;
+                      case TaskItemEnum.delete:
+                        deleteTask(widget.tasks[index].id);
+                        break;
+                    }
+                  },
+                  child: Icon(
                     Icons.more_vert,
                     color:
-                        tasks[index].isDone
+                        widget.tasks[index].isDone
                             ? Theme.of(context).cardColor
                             : Theme.of(context).primaryColorDark,
                   ),
                 ),
+
                 Gap(5),
               ],
             ),
